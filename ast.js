54113e3e75f59ae52225481ccd6646ece7d56375
j-joker <zlxjune@gmail.com>
2020-04-11 14:59:53 +0800

    printer

---
+++ ast.js
@@ -0,0 +1,44 @@
+const process = require('process');
+const fs = require('fs')
+function print(x){
+	process.stdout.write(x.toString())
+}
+
+class Expr {
+
+}
+class BinaryExpr {
+	constructor(op,left,right){
+		this.left=left;
+		this.op=op;
+		this.right=right;
+	}
+}
+
+class UnaryExpr {
+	constructor(op,expr){
+		this.op=op;
+		this.expr=expr;
+	}
+}
+
+function printer(expr){
+	if(expr instanceof BinaryExpr){
+		let {op ,left,right}=expr;
+		print("(")
+		printer(op)
+		printer(left)
+		printer(right)
+		print(")")
+	}
+	else if(expr instanceof UnaryExpr){
+		printer(expr.op)
+		printer(expr.expr)
+	}
+	else{
+		print(expr+" ")
+	}
+}
+
+let x=new BinaryExpr('+',1,2)
+printer(x)
--- main.js
+++ main.js
@@ -7,9 +7,16 @@ let TokenTypes = {
 	SLASH:6,
 	EQUAL_EQUAL:7,
 	STRING:8,
-	NUM:9
+	NUM:9,
+	// keywords
+	AND:10,
+	IF:11
 
 
+}
+const keywords={
+	and:TokenTypes.AND,
+	if:TokenTypes.IF
 }
 class Scanner {
 	constructor(source){
@@ -32,10 +39,10 @@ class Scanner {
 			case '+':addToken(TokenTypes.PLUS,null);break;
 			case '-':addToken(TokenTypes.MINUS,null);break;
 			// 需要向前看才能处理的符号
-			case '=':addToken(match('=') ? TokenTypes.EQUAL_EQUAL : TokenTypes.EQUAL);break;
+			case '=':addToken(this.ifMatchKeepMove('=') ? TokenTypes.EQUAL_EQUAL : TokenTypes.EQUAL);break;
 			case '/':
-				if(match('/')){
-					while(unmatch('\n') || !isEnd());
+				if(this.ifMatchKeepMove('/')){
+					while(this.ifUnmatchKeepMove('\n') || !isEnd());
 				}else{
 					addToken(TokenTypes.SLASH)
 				}
@@ -43,6 +50,7 @@ class Scanner {
 			case '\n':
 			case '\t':
 			case '\r':
+				this.current++;
 				break;
 
 			case '"':
@@ -70,7 +78,7 @@ class Scanner {
 		this.addToken(TokenTypes.NUM,parseInt(intVal))
 	}
 	string(){
-		while(this.unmatch('"'));
+		while(this.ifUnmatchKeepMove('"'));
 		let str=this.source.substring(this.start+1,this.current)
 		this.current++;
 		this.addToken(TokenTypes.STRING,str)
@@ -82,14 +90,14 @@ class Scanner {
 	advance(){
 		return this.source[this.current++]
 	}
-	match(x){
+	ifMatchKeepMove(x){
 		if(this.source[current] === x){
 			current++;
 			return true;
 		}
 		return false;
 	}
-	unmatch(x){
+	ifUnmatchKeepMove(x){
 		if(this.source[this.current] !== x){
 			this.current++;
 			return true;
@@ -110,7 +118,11 @@ class Token {
 		this.type=type
 	}
 }
-let code="\"abc\""
+
+
+
+// let code="\"abc\""
+let code="12"
 let s=new Scanner(code)
 s.scanTokens()
 console.log(s.tokens)
